f60e3b7d11a98e691ac13f035a554bf8a775c2f9
okunotomoya <gomakuro25@gmail.com>
2025-11-23 16:27:20 +0900

    Fixes and updates based on reviewer comments

--- kcat_prediction_slim/visualize_box_esm2.py
+++ kcat_prediction_slim/visualize_box_esm2.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import warnings
 from enum import Enum
 from pathlib import Path
@@ -6,13 +7,12 @@ from typing import List, Set
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from scipy.stats import wilcoxon
+from sklearn.metrics import r2_score
 
 from kcat_prediction_slim.default_path import DefaultPath
-from scipy.stats import wilcoxon
-import itertools
-import numpy as np
-from sklearn.metrics import r2_score
 
 warnings.filterwarnings("ignore")
 
@@ -40,9 +40,9 @@ model_names = {
     "ESM1b": "ESM-1b$_\\mathrm{{MEAN}}$",
     "DRFP": "DRFP",
     "ESM1b_ts": "ESM-1b$_\\mathrm{{ESP}}$",
-    "ESM1b_ts_diff_fp": "ESM-1b$_\\mathrm{{ESP}}$\n + diff. FP",
-    "ESM1b_ts_DRFP": "ESM-1b$_\\mathrm{{ESP}}$\n + DRFP",
-    "ESM1b_ts_DRFP_mean": "ESM-1b$_\\mathrm{{ESP}}$     \n + DRFP (mean)",
+    "ESM1b_ts_diff_fp": "ESM-1b$_\\mathrm{{ESP}}$ + diff. FP",
+    "ESM1b_ts_DRFP": "ESM-1b$_\\mathrm{{ESP}}$ + DRFP",
+    "ESM1b_ts_DRFP_mean": "ESM-1b$_\\mathrm{{ESP}}$ + DRFP (mean)",
     #
     # "ESM1B_EnzSRP": "ESM1B EnzSRP",
     # "ESM1B_EnzSRP_DRFP": "ESM1B EnzSRP\n + DRFP",
@@ -52,14 +52,18 @@ model_names = {
 
 def load_result(result_dirs, models, seeds):
     model_results = {}
+    added = set()
     for i, model in enumerate(models):
         results = []
         for seed in seeds:
             for result_dir in result_dirs:
                 try:
+                    if (model, seed) in added:
+                        continue
                     pred_y = np.load(result_dir / f"y_test_pred_xgboost_{model}_{seed}.npy")
                     test_y = np.load(result_dir / f"y_test_true_xgboost_{model}_{seed}.npy")
                     results.append((pred_y, test_y))
+                    added.add((model, seed))
                 except Exception as e:
                     pass
         model_results[model] = results
@@ -69,8 +73,8 @@ def load_result(result_dirs, models, seeds):
 def filtered_unfiltered_indexes(test_y):
     data_test = pd.read_pickle(
         DefaultPath().original_data_dir / "kcat_data" / "splits" / "test_df_kcat.pkl")
-    data_test_filtered = pd.read_csv(
-        DefaultPath().project_root / "data" / "test_df_kcat_filtered_sequence_ids.csv")
+    data_test_filtered = pd.read_pickle(
+        DefaultPath().project_root / "data" / "test_df_kcat_filtered.pkl")
 
     assert np.all(np.isclose(test_y, np.array(data_test['geomean_kcat']), rtol=1e-5, atol=1e-8)), "Arrays differ"
 
@@ -109,15 +113,43 @@ def load_unfiltered_result(result_dirs, models, seeds):
 #     temp = text.replace('ESM1B_EnzSRP_kcat_sequence_embeddings', '')
 #     temp = temp.replace('ESM1B_EnzSRP_DRFP_kcat_sequence_embeddings', '\n + DRFP (mean)')
 #     return temp
+
+model_suffix_map = {
+    '250420_121652': '',
+    '251022_154842': ' (mask_$0.15$)',
+    '251025_170705': ' (lr_$3e^{-4}$)',
+    '251026_164701': ' (lr_$1e^{-4}$)',
+    'esm2_mean': ' esm2_mean',
+    '251031_092936': '',
+}
+
+
+# suffix_for_esm = ['', '_mask_$0.15$', '_lr_$3e^{-4}$', '_lr_$1e^{-4}$', '_esm2', '_esm2-adapted']
+# suffix_for_esm = [*suffix_for_esm, *suffix_for_esm]
 def clean_label(text: str):
-    if 'ESM1B_EnzSRP_kcat_sequence_embeddings' in text:
-        return 'ESM-1b$_\\mathrm{{DA}}$'
-    if 'ESM1B_EnzSRP_DRFP_mean_kcat_sequence_embeddings' in text:
-        return 'ESM-1b$_\\mathrm{{DA}}$     \n + DRFP (mean)'
+    global suffix_count
+    if 'esm2' in text:
+        return f"ESM-2$_\\mathrm{{MEAN}}$"
+    if 'ESM_EnzSRP' in text:
+        model_name = '_'.join(text.split('_')[-2:])
+        if 'ESM_EnzSRP_kcat_sequence_embeddings' in text:
+            suffix = model_suffix_map[model_name]
+            return f"ESM-2$_\\mathrm{{DA}}${suffix}"
+        if 'ESM_EnzSRP_DRFP_mean_kcat_sequence_embeddings' in text:
+            suffix = model_suffix_map[model_name]
+            return f"ESM-2$_\\mathrm{{DA}}${suffix} + DRFP (mean)"
+    elif 'ESM1B_EnzSRP' in text:
+        model_name = '_'.join(text.split('_')[-2:])
+        if 'ESM1B_EnzSRP_kcat_sequence_embeddings' in text:
+            suffix = model_suffix_map[model_name]
+            return f"ESM-1b$_\\mathrm{{DA}}${suffix}"
+        if 'ESM1B_EnzSRP_DRFP_mean_kcat_sequence_embeddings' in text:
+            suffix = model_suffix_map[model_name]
+            return f"ESM-1b$_\\mathrm{{DA}}${suffix} + DRFP (mean)"
     return text
 
 
-def show_mse(ax, results):
+def show_mse(ax, results, labels):
     # ax.set_ylim(0.7, 1.0)
     # ax.set_xlim(0.5, len(models) + 0.5)
     box_data = []
@@ -125,7 +157,7 @@ def show_mse(ax, results):
         model_result = [np.mean(abs(test_y - pred_y) ** 2) for pred_y, test_y in seed_results]
         box_data.append(model_result)
     ax.boxplot(box_data)
-    ax.set_xticklabels(results.keys(), rotation=45, ha='right', rotation_mode='anchor')
+    ax.set_xticklabels(labels, rotation=30, ha='right', rotation_mode='anchor')
     for label in ax.get_xticklabels():
         if '\n' in label._text:
             label.set_va('center')
@@ -166,13 +198,13 @@ def run_wilcoxon_for_coefficient_of_determination(results):
         print(f"{marker} {cond1} vs {cond2}: stat = {stat:.3f}, p = {p_val:.3g}")
 
 
-def show_coefficient_of_determination(ax, results):
+def show_coefficient_of_determination(ax, results, labels):
     box_data = []
     for i, (model, seed_results) in enumerate(results.items()):
         model_result = [r2_score(test_y, pred_y) for pred_y, test_y in seed_results]
         box_data.append(model_result)
     ax.boxplot(box_data)
-    ax.set_xticklabels(results.keys(), rotation=45, ha='right', rotation_mode='anchor')
+    ax.set_xticklabels(labels, rotation=30, ha='right', rotation_mode='anchor')
     for label in ax.get_xticklabels():
         if '\n' in label._text:
             label.set_va('center')
@@ -194,57 +226,43 @@ class DrawType(Enum):
 
 
 def main(target_ver: str, x_models: Set[str], x_seeds: Set[int], draw_type: DrawType,
-         results_parent_dir: Path,
-         model_name_prefix='ESM1B_EnzSRP_kcat_sequence_embeddings',
-         model_name_prefix2='ESM1B_EnzSRP_DRFP_mean_kcat_sequence_embeddings', out_file_stem='boxplots'):
-    x_models = [*fixed_models,
-                *[f"{model_name_prefix}_{n}" for n in x_models],
-                *fixed_models_drfp_mean,
-                *[f"{model_name_prefix2}_{n}" for n in x_models]]
+         out_file_stem='boxplots'):
 
+    x_models = ['250420_121652']
+    y_models = ['251031_092936']
+    x_models = ["DRFP",
+                "ESM1b",
+                f"ESM_EnzSRP_kcat_sequence_embeddings_esm2_mean",
+                *[f"ESM1B_EnzSRP_kcat_sequence_embeddings_{n}" for n in x_models],
+                *[f"ESM_EnzSRP_kcat_sequence_embeddings_{n}" for n in y_models],
+                *[f"ESM1B_EnzSRP_DRFP_mean_kcat_sequence_embeddings_{n}" for n in x_models],
+                *[f"ESM_EnzSRP_DRFP_mean_kcat_sequence_embeddings_{n}" for n in y_models]]
+
+    result_parent_dir = DefaultPath().build / 'training_results'
     out_dir = DefaultPath().build / 'viz' / target_ver
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    dirs = list_subdirectories(results_parent_dir / target_ver)
-    if draw_type == DrawType.ALL:
-        results = load_result(dirs, x_models, x_seeds)
-    elif draw_type == DrawType.FILTERED:
-        results = load_filtered_result(dirs, x_models, x_seeds)
-    elif draw_type == DrawType.UNFILTERED:
-        results = load_unfiltered_result(dirs, x_models, x_seeds)
-    else:
-        raise ValueError('unexpected')
-    label_map = {model: model_names[model] if model in model_names.keys() else clean_label(model) for model in x_models}
+    dirs = list_subdirectories(result_parent_dir / target_ver)
+    results = load_result(dirs, x_models, x_seeds)
+    labels = [model_names[model] if model in model_names.keys() else clean_label(model) for model in x_models]
 
-    results = {label_map[k]: v for k, v in results.items()}
-
-    fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+    fig, axes = plt.subplots(2, 1, figsize=(10, 14))
     fig.subplots_adjust(bottom=0.3)
-    show_coefficient_of_determination(axes[0], results)
+    show_coefficient_of_determination(axes[0], results, labels)
     run_wilcoxon_for_coefficient_of_determination(results)
-    show_mse(axes[1], results)
+    show_mse(axes[1], results, labels)
     run_wilcoxon_for_mse(results)
     plt.savefig(out_dir / f"{out_file_stem}.png")
     plt.savefig(out_dir / f"{out_file_stem}.pdf")
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--target-app-ver', type=str, default='v2_1_0')
-    parser.add_argument('--models', nargs='+', default=['250420_121652'])
+    parser.add_argument('--target-app-ver', type=str, default='esm2')
+    parser.add_argument('--models', nargs='+', default=['esm2_mean', '250420_121652', '251031_092936'])
     parser.add_argument('--seeds', type=int, nargs='+', default=[42, 43, 44, 45, 46, 47, 48, 49, 50, 51])
-    parser.add_argument('--result-parent-dir', type=Path, nargs=None, default='../build/training_results')
-
-    # result_parent_dir = DefaultPath().build / 'server_training_results'
 
     args = parser.parse_args()
-    print('boxplot_all')
     main(target_ver=args.target_app_ver, x_models=args.models, x_seeds=args.seeds, draw_type=DrawType.ALL,
-         out_file_stem='boxplot_all', results_parent_dir=args.result_parent_dir)
-    print('boxplot_filtered')
-    main(target_ver=args.target_app_ver, x_models=args.models, x_seeds=args.seeds, draw_type=DrawType.FILTERED,
-         out_file_stem='boxplot_filtered', results_parent_dir=args.result_parent_dir)
-    print('boxplot_unfiltered')
-    main(target_ver=args.target_app_ver, x_models=args.models, x_seeds=args.seeds, draw_type=DrawType.UNFILTERED,
-         out_file_stem='boxplot_unfiltered', results_parent_dir=args.result_parent_dir)
+         out_file_stem='boxplot_esm2')
